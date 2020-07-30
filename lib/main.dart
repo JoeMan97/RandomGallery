42fc530ac97280d37de2a3c71710b875ac609ca1
Jose Carrillo <joseemanueelct@gmail.com>
2020-07-30 17:58:47 -0500

    Scroll infinito

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
@@ -13,6 +15,42 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final List<String> dogImages = new List();
+  ScrollController _scrollController = new ScrollController();
+
+  fetchImages() async {
+    Response response = await get('https://dog.ceo/api/breeds/image/random');
+    if (response.statusCode == 200) {
+      setState(() {
+        dogImages.add(json.decode(response.body)['message']);
+      });
+    } else {
+      throw Exception('No se pudo cargar las imágenes');
+    }
+  }
+
+  fetchFour() {
+    for (int i = 0; i < 4; i++) {
+      fetchImages();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFour();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        fetchFour();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +61,11 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: ListView.builder(
+        controller: _scrollController,
         itemCount: dogImages.length,
         itemBuilder: (context , index) {
           return Container(
-            constraints: BoxConstraints.tightFor(height: 150),
+            constraints: BoxConstraints.tightFor(height: 200),
             child: Image.network(dogImages[index], fit: BoxFit.fitWidth),
           );
         },
