5f4c637906b46eab95a1ce0ade71ab500e4ce8ba
Jose Carrillo <joseemanueelct@gmail.com>
2020-08-03 08:57:10 -0500

    refactor con provider!

--- lib/classes/dogs_images.dart
+++ lib/classes/dogs_images.dart
@@ -1,16 +1,19 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
-class DogsImages {
+class DogsImages with ChangeNotifier {
   List<List<String>> images = new List();
 
-  _fetchImages(int index) async {
+  void _fetchThree() async {
     List<String> imagesAux = new List(3);
-
     Response response1 = await get('https://dog.ceo/api/breeds/image/random');
     Response response2 = await get('https://dog.ceo/api/breeds/image/random');
     Response response3 = await get('https://dog.ceo/api/breeds/image/random');
-    if (response1.statusCode == 200 && response2.statusCode == 200 && response3.statusCode == 200) {
+
+    if (response1.statusCode == 200 &&
+        response2.statusCode == 200 &&
+        response3.statusCode == 200) {
       imagesAux[0] = (json.decode(response1.body)['message']);
       imagesAux[1] = (json.decode(response2.body)['message']);
       imagesAux[2] = (json.decode(response3.body)['message']);
@@ -19,11 +22,12 @@ class DogsImages {
     }
 
     images.add(imagesAux);
+    notifyListeners();
   }
 
-  fetchTwelve() {
+  void fetchTwelve() {
     for (int i = 0; i < 4; i++) {
-      _fetchImages(i);
+      _fetchThree();
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:random_gallery/screensize_reducers.dart';
-import 'package:random_gallery/dogs_images.dart';
+import 'package:provider/provider.dart';
+import 'package:random_gallery/classes/dogs_images.dart';
+import 'package:random_gallery/widgets/images_listview.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: ChangeNotifierProvider<DogsImages>( 
+        create: (context) => DogsImages(),
+        child: Home(),
+    ),
   ));
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  DogsImages dogsImages = new DogsImages();
-  ScrollController _scrollController = new ScrollController();
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    setState(() {
-      dogsImages.fetchTwelve();  
-    });
-
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-        setState(() {
-          dogsImages.fetchTwelve();  
-        });
-      }
-    });
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dogsImages = Provider.of<DogsImages>(context, listen: false);
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.indigo[700],
         title: Text('RandomGallery'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        controller: _scrollController,
-        itemCount: dogsImages.images.length,
-        itemBuilder: (context, index) => 
-          Container(
-            margin: EdgeInsets.only(top: 8),
-            child: CarouselSlider(
-              options: CarouselOptions(
-                enlargeCenterPage: true,
-                aspectRatio: 16/9,
-                autoPlayCurve: Curves.fastOutSlowIn,
-                height: 
-                ScreensizeReducers().screenHeightExcludingToolbar(
-                  context,
-                  dividedBy: 4
-                ) - 12.0, // no se activa el evento si coloco 16
-                enableInfiniteScroll: true,
-                viewportFraction: 0.8
-              ),
-              items: 
-              dogsImages.images[index].map((image) => 
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: Image.network('$image')
-                ),
-              ).toList()
-            ),
-          )
-      )
+      backgroundColor: Colors.indigo[50],
+      body: ImagesListView(dogsImages: dogsImages),
     );
   }
 }
+
